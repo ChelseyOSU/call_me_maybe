791a88b609d1bdfbac4a7e36c6d502491a731abc
ChelseyOSU <xiaoshieducation@gmail.com>
2022-02-06 22:44:37 -0800

    add launch_url for sms and github link

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:io';
 import '../models/profile.dart';
 
-class ProfileScreen extends StatelessWidget {
+const String _url = 'https://github.com/ChelseyOSU';
+const String _androidPhoneNumber = '+1 706 201 4495?body=hello%20there';
+const String _iosPhoneNumber = '17062014495&body=hello%20there';
+const String _emailAddress='zengxi@oregonstate.edu';
+
+
+class ProfileScreen extends StatelessWidget {  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -9,7 +17,32 @@ class ProfileScreen extends StatelessWidget {
       children: [
         SizedBox(child: profileImage(), height:200),
         SizedBox(child: const Text("Student at Oregon State Unversity"), height:300),
-        SizedBox(child: const Text("zengxi@oregonstate.edu"), height: 200)
+        ElevatedButton(
+          onPressed: _lanuchSMS,
+          child: Text("706-201-4495")
+          ),
+        Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+            ElevatedButton(
+              onPressed: _launchURL,
+              child: Text(_url)
+            ),
+            Text(_emailAddress)
+           ], 
+        )
       ]);
+  }
+}
+
+void _launchURL() async {
+  if (!await launch(_url)) throw 'Could not connect to $_url';
+}
+
+void _lanuchSMS() async {
+  if (Platform.isAndroid) {
+    if (!await launch("sms:${_androidPhoneNumber}")) throw 'Could not connect to android messages';
+  } else if (Platform.isIOS) {
+    if (!await launch("sms:${_iosPhoneNumber}")) throw 'Could not connect to iMessages';
   }
 }
